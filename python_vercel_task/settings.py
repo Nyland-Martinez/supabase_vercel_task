ca5d1c6e7313561de234802e9471f6378dd49034
Nyland-Martinez <martinezhaidee95@gmail.com>
2024-11-19 19:27:08 -0300

    archivo .env

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'python_vercel_task.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': config('DB_ENGINE'),
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
